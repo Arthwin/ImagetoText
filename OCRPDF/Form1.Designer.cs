4bfc063bdf7b0ee3c41c291cc1564c8593b03cd6
Arthwin <dan_olarte@hotmail.com>
2017-04-24 15:25:45 -0400

    Finaly works

---
+++ OCRPDF/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace OCRPDF
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtExtracted = new System.Windows.Forms.TextBox();
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.pbCrop = new System.Windows.Forms.PictureBox();
+            this.btnPage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCrop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 4);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 9;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(93, 4);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(168, 20);
+            this.txtPath.TabIndex = 8;
+            // 
+            // txtExtracted
+            // 
+            this.txtExtracted.Location = new System.Drawing.Point(9, 285);
+            this.txtExtracted.Multiline = true;
+            this.txtExtracted.Name = "txtExtracted";
+            this.txtExtracted.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtExtracted.Size = new System.Drawing.Size(250, 250);
+            this.txtExtracted.TabIndex = 7;
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOriginal.Location = new System.Drawing.Point(9, 32);
+            this.pbOriginal.Margin = new System.Windows.Forms.Padding(0);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(250, 250);
+            this.pbOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbOriginal.TabIndex = 6;
+            this.pbOriginal.TabStop = false;
+            this.pbOriginal.Paint += new System.Windows.Forms.PaintEventHandler(this.pbOriginal_Paint);
+            this.pbOriginal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbOriginal_MouseDown);
+            this.pbOriginal.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbOriginal_MouseMove);
+            this.pbOriginal.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbOriginal_MouseUp);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(688, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 26);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate!";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // pbCrop
+            // 
+            this.pbCrop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCrop.Location = new System.Drawing.Point(267, 32);
+            this.pbCrop.Margin = new System.Windows.Forms.Padding(0);
+            this.pbCrop.Name = "pbCrop";
+            this.pbCrop.Size = new System.Drawing.Size(500, 500);
+            this.pbCrop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCrop.TabIndex = 10;
+            this.pbCrop.TabStop = false;
+            this.pbCrop.Paint += new System.Windows.Forms.PaintEventHandler(this.pbCrop_Paint);
+            this.pbCrop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbCrop_MouseDown);
+            this.pbCrop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbCrop_MouseMove);
+            this.pbCrop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbCrop_MouseUp);
+            // 
+            // btnPage
+            // 
+            this.btnPage.Location = new System.Drawing.Point(267, 1);
+            this.btnPage.Name = "btnPage";
+            this.btnPage.Size = new System.Drawing.Size(75, 26);
+            this.btnPage.TabIndex = 11;
+            this.btnPage.Text = "Next Page";
+            this.btnPage.UseVisualStyleBackColor = true;
+            this.btnPage.Click += new System.EventHandler(this.btnPage_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 540);
+            this.Controls.Add(this.pbCrop);
+            this.Controls.Add(this.btnPage);
+            this.Controls.Add(this.pbOriginal);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtExtracted);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmMain";
+            this.Text = "PDF OCR";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCrop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtExtracted;
+        protected System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.Button btnCalculate;
+        protected System.Windows.Forms.PictureBox pbCrop;
+        private System.Windows.Forms.Button btnPage;
+    }
+}
+
---
+++ OCRPDF/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace OCRPDF
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            //Delete temp files
+            var di = new DirectoryInfo(@"temp\");
+            foreach (var file in di.GetFiles())
+                file.Delete();
+            InitializeComponent();
+        }
+
+        #region Common Functions
+
+        private static void RunCmd(string command)
+        {
+            //http://stackoverflow.com/questions/1469764/run-command-prompt-commands
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = "/C " + command
+            };
+            var process = new System.Diagnostics.Process { StartInfo = startInfo };
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+            process.Dispose();
+        }
+
+        private void SetPictureBox(string picture)
+        {
+            var i = Image.FromFile(picture);
+            i.Save(@"temp\temp.png");
+            if (i.Width < i.Height)
+            {
+                pbOriginal.Height = 250;
+                pbOriginal.Width = 250 * i.Width / i.Height;
+            }
+            else
+            {
+                pbOriginal.Width = 250;
+                pbOriginal.Height = 250 * i.Height / i.Width;
+            }
+            i.Dispose();
+            using (var stream = new FileStream(@"temp\temp.png", FileMode.Open, FileAccess.Read))
+                pbOriginal.Image = Image.FromStream(stream);
+            _cropRectangle = new Rectangle(0, 0, 0, 0);
+            _cropStart = new Point(0, 0);
+            PaintCropped(@"temp\temp.png");
+            pbOriginal.Invalidate();
+        }
+
+        private void PaintCropped(string picture)
+        {
+            var i = Image.FromFile(picture);
+            if (picture != @"temp\temp2.png")
+                i.Save(@"temp\temp2.png");
+            if (i.Width < i.Height)
+            {
+                pbCrop.Height = 500;
+                pbCrop.Width = 500 * i.Width / i.Height;
+            }
+            else
+            {
+                pbCrop.Width = 500;
+                pbCrop.Height = 500 * i.Height / i.Width;
+            }
+            i.Dispose();
+            using (var stream = new FileStream(@"temp\temp2.png", FileMode.Open, FileAccess.Read))
+                pbCrop.Image = Image.FromStream(stream);
+            _cropRectangle2 = new Rectangle(0, 0, 0, 0);
+            _cropStart2 = new Point(0, 0);
+            pbCrop.Invalidate();
+            txtExtracted.Text = "";
+        }
+
+        #endregion
+
+        #region Buttons
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            //Ask for file
+            var openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                Filter = "All files (*.*)|*.*|pdf files (*.pdf)|*.pdf",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            var myStream = openFileDialog1.OpenFile();
+            //Delete temp files
+            var di = new DirectoryInfo(@"temp\");
+            foreach (var file in di.GetFiles())
+                file.Delete();
+            using (myStream)
+            {
+                //Disable interaction while processing
+                btnCalculate.Enabled = false;
+                btnOpenFile.Enabled = false;
+                btnPage.Enabled = false;
+                //Pdf to Png
+                RunCmd("magick -density 300 " + openFileDialog1.FileName +
+                       " " + Directory.GetCurrentDirectory() + @"\temp\pdf.png");
+                //Set pictureboxes
+                SetPictureBox(@"temp\" + new DirectoryInfo(@"temp\").GetFiles("pdf*.png")[0]);
+                //Reset extarcted text
+                txtExtracted.Text = "";
+                //Set paths
+                txtPath.Text = openFileDialog1.FileName;
+                //Reset interaction
+                btnPage.Enabled = true;
+                btnCalculate.Enabled = true;
+                btnOpenFile.Enabled = true;
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Dont calculate if no file selected
+            if (txtPath.Text == "") return;
+            //Disable interaction while processing
+            btnCalculate.Enabled = false;
+            btnOpenFile.Enabled = false;
+            btnPage.Enabled = false;
+            //OCR
+            RunCmd("tesseract " + Directory.GetCurrentDirectory() + @"\temp\temp2.png " + 
+                    Directory.GetCurrentDirectory() + @"\temp\out ");
+            //Grab text, insert correct endlines, remove extra ones
+            txtExtracted.Text = Regex.Replace(File.ReadAllText(Directory.GetCurrentDirectory()
+                                + @"\temp\out.txt", Encoding.UTF8).Replace("\n", "\r\n"), @"[\r\n]+",
+                                "\r\n").TrimEnd('\r', '\n');
+            //Reset interaction
+            btnPage.Enabled = true;
+            btnCalculate.Enabled = true;
+            btnOpenFile.Enabled = true;
+        }
+
+        private int _current;
+        private void btnPage_Click(object sender, EventArgs e)
+        {
+            //Set pictureboxes
+            _current++;
+            try
+            {
+                SetPictureBox(@"temp\" + new DirectoryInfo(@"temp\").GetFiles("pdf*.png")[_current]);
+            }
+            catch (Exception exception)
+            {
+                _current = 0;
+                try
+                {
+                    SetPictureBox(@"temp\" + new DirectoryInfo(@"temp\").GetFiles("pdf*.png")[_current]);
+                }
+                catch (Exception exception2)
+                {
+                    pbCrop.Image = null;
+                    pbCrop.Width = 500;
+                    pbCrop.Height = 500;
+                    pbOriginal.Image = null;
+                    pbOriginal.Width = 250;
+                    pbOriginal.Height = 250;
+                    txtExtracted.Text = "";
+                    txtPath.Text = "";
+                }
+            }
+        }
+
+        #endregion
+
+        #region pbOriginal Events
+
+        private Rectangle _cropRectangle;
+        private Point _cropStart;
+        private bool _isDragging;
+        private bool _moved;
+
+        private void pbOriginal_MouseDown(object sender, MouseEventArgs e)
+        {
+            //if n ofile is selected, leave
+            if (txtPath.Text == "") return;
+            //Reset moved variable
+            _moved = false;
+            if (e.Button == MouseButtons.Left)
+            {
+                //if left click, prepare to drag
+                _cropRectangle = new Rectangle(e.X, e.Y, 0, 0);
+                _cropStart = new Point(e.X, e.Y);
+                _isDragging = true;
+            }
+            else
+            {
+                //if right click, reset crop
+                _cropRectangle = new Rectangle(0, 0, 0, 0);
+                _cropStart = new Point(0, 0);
+                pbOriginal.Invalidate();
+                PaintCropped(@"temp\temp.png");
+            }
+        }
+
+        private void pbOriginal_MouseUp(object sender, MouseEventArgs e)
+        {
+            //exit if no file or right click
+            if (e.Button != MouseButtons.Left || txtPath.Text == "") return;
+            if (_moved)
+            {
+                try
+                {
+                    //Done selecting crop rectangle, copy to new bitmap
+                    var bmpImage = new Bitmap(@"temp\temp.png");
+                    var croppedImage = bmpImage.Clone(
+                        new Rectangle(_cropRectangle.X * bmpImage.Width / pbOriginal.Width,
+                                        _cropRectangle.Y * bmpImage.Height / pbOriginal.Height,
+                                        _cropRectangle.Width * bmpImage.Width / pbOriginal.Width,
+                                        _cropRectangle.Height * bmpImage.Height / pbOriginal.Height),
+                                        bmpImage.PixelFormat);
+                    bmpImage.Dispose();
+                    //save to new temp and paint in picture box
+                    croppedImage.Save(@"temp\temp2.png");
+                    croppedImage.Dispose();
+                    PaintCropped(@"temp\temp2.png");
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+            else
+            {
+                //if it didnt move, reset selection
+                _cropRectangle = new Rectangle(0, 0, 0, 0);
+                _cropStart = new Point(0, 0);
+                pbOriginal.Invalidate();
+                PaintCropped(@"temp\temp.png");
+            }
+            //Reset variables
+            _moved = false;
+            _isDragging = false;
+        }
+
+        private void pbOriginal_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if they didnt left click before, leave
+            if (!_isDragging) return;
+            //tell methods mouse dragged
+            _moved = true;
+            //Make sure the selection is limited to the controlbox
+            var x = e.X > pbOriginal.Width ? pbOriginal.Width - 4 : e.X;
+            var y = e.Y > pbOriginal.Height ? pbOriginal.Height - 4 : e.Y;
+            x = x >= 2 ? x : 2;
+            y = y >= 2 ? y : 2;
+            //if it didnt move a considerable amount, move it one pixel
+            var w = Math.Abs(x - _cropStart.X) == 0? 2: Math.Abs(x - _cropStart.X);
+            var h = Math.Abs(y - _cropStart.Y) == 0? 2: Math.Abs(y - _cropStart.Y);
+            //Create the new crop rectangle
+            _cropRectangle = new Rectangle(Math.Min(_cropStart.X, x),
+                                           Math.Min(_cropStart.Y, y), w, h);
+            //redraw
+            pbOriginal.Invalidate();
+        }
+
+        private void pbOriginal_Paint(object sender, PaintEventArgs e)
+        {
+            //Paint selection rectangle
+            e.Graphics.DrawRectangle(Pens.Red, _cropRectangle);
+        }
+
+        #endregion
+
+        #region pbCrop Events
+
+        private Rectangle _cropRectangle2;
+        private Point _cropStart2;
+        private bool _isDragging2;
+        private bool _moved2;
+
+        private void pbCrop_MouseDown(object sender, MouseEventArgs e)
+        {
+            //if n ofile is selected, leave
+            if (txtPath.Text == "") return;
+            //Reset moved variable
+            _moved2 = false;
+            if (e.Button == MouseButtons.Left)
+            {
+                //if left click, prepare to drag
+                _cropRectangle2 = new Rectangle(e.X, e.Y, 0, 0);
+                _cropStart2 = new Point(e.X, e.Y);
+                _isDragging2 = true;
+            }
+            else
+            {
+                //if right click, reset crop
+                PaintCropped(@"temp\temp2.png");
+            }
+        }
+
+        private void pbCrop_MouseUp(object sender, MouseEventArgs e)
+        {
+            //exit if no file or right click
+            if (e.Button != MouseButtons.Left || txtPath.Text == "") return;
+            if (_moved2)
+            {
+                try
+                {
+                    //Done selecting crop rectangle, copy to new bitmap
+                    var bmpImage = new Bitmap(@"temp\temp2.png");
+                    var croppedImage = bmpImage.Clone(
+                        new Rectangle(_cropRectangle2.X * bmpImage.Width / pbCrop.Width,
+                                        _cropRectangle2.Y * bmpImage.Height / pbCrop.Height,
+                                        _cropRectangle2.Width * bmpImage.Width / pbCrop.Width,
+                                        _cropRectangle2.Height * bmpImage.Height / pbCrop.Height),
+                                        bmpImage.PixelFormat);
+                    bmpImage.Dispose();
+                    //save to new temp and paint in picture box
+                    croppedImage.Save(@"temp\temp2.png");
+                    croppedImage.Dispose();
+                    PaintCropped(@"temp\temp2.png");
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+            else
+            {
+                //if it didnt move, reset selection
+                PaintCropped(@"temp\temp2.png");
+            }
+            //Reset variables
+            _moved2 = false;
+            _isDragging2 = false;
+        }
+
+        private void pbCrop_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if they didnt left click before, leave
+            if (!_isDragging2) return;
+            //tell methods mouse dragged
+            _moved2 = true;
+            //Make sure the selection is limited to the controlbox
+            var x = e.X > pbCrop.Width ? pbCrop.Width - 4 : e.X;
+            var y = e.Y > pbCrop.Height ? pbCrop.Height - 4 : e.Y;
+            x = x >= 2 ? x : 2;
+            y = y >= 2 ? y : 2;
+            //if it didnt move a considerable amount, move it one pixel
+            var w = Math.Abs(x - _cropStart2.X) == 0 ? 2 : Math.Abs(x - _cropStart2.X);
+            var h = Math.Abs(y - _cropStart2.Y) == 0 ? 2 : Math.Abs(y - _cropStart2.Y);
+            //Create the new crop rectangle
+            _cropRectangle2 = new Rectangle(Math.Min(_cropStart2.X, x),
+                                            Math.Min(_cropStart2.Y, y), w, h);
+            //redraw
+            pbCrop.Invalidate();
+        }
+
+        private void pbCrop_Paint(object sender, PaintEventArgs e)
+        {
+            //Paint selection rectangle
+            e.Graphics.DrawRectangle(Pens.Red, _cropRectangle2);
+            //reset selection
+            _cropRectangle = new Rectangle(0, 0, 0, 0);
+            _cropStart = new Point(0, 0);
+            pbOriginal.Invalidate();
+        }
+
+        #endregion
+
+    }
+}
